3dc90b29f3a46ce4a722810099a989cc79f70910
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-08-30 23:28:50 -0700

    :rocket: Serverless API AWS

---
+++ README.md
@@ -0,0 +1,3 @@
+# serverless-api
+
+[People Routes](https://us-west-2.console.aws.amazon.com/apigateway/home?region=us-west-2#/apis/fk7ww8l4z8/resources/wtuiec)
---
+++ handleDeletePeople/index.js
@@ -0,0 +1,31 @@
+const dynamoose = require('dynamoose');
+
+// make a schema - just like we did with mongoDB or like with our sql db
+const schema = new dynamoose.Schema({ id: String, name: String, age: Number });
+
+const peopleModel = dynamoose.model('people', schema);
+
+exports.handler = async (event) => {
+  const response = {
+    statusCode: null,
+    body: null,
+  };
+  try {
+    const id = event.pathParameters?.id;
+    let results;
+    if (id) {
+      const list = await peopleModel.query('id').eq(id).exec();
+      results = list[0];
+    } else {
+      results = await peopleModel.scan().exec();
+    }
+    response.statusCode = 200;
+    response.body = JSON.stringify(results);
+  } catch (e) {
+    // if I am not successful in getting from my table I will send back status 500 and the error message
+    response.statusCode = 500;
+    response.body = JSON.stringify(e.message);
+  }
+
+  return response;
+};
